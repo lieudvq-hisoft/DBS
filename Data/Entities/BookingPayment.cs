8186529fad36054e39fb1a48befdbac40aa162bd
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-04-09 23:29:29 +0700

    Update BookingPayment

--- Data/Entities/BookingPayment.cs
+++ Data/Entities/BookingPayment.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Data.Entities;
 
 public class BookingPayment : BaseEntity
 {
     public Guid BookingId { get; set; }
+    [ForeignKey("BookingId")]
     public virtual Booking? Booking { get; set; }
     public long Amount { get; set; }
     public bool IsPaid { get; set; } = false;
