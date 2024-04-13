ca833be191ac837a65b79ad4e5dbb232ed44b4a8
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-04-13 15:57:05 +0700

    Feat

--- Data/Models/UserModels.cs
+++ Data/Models/UserModels.cs
@@ -34,7 +34,7 @@ namespace Data.Model
 
     public class RegisterDriverByAdminModel
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
