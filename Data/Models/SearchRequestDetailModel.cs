2a2001c66a287a7ff7e8bbb16a7d94ba5c49bb7a
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-04-15 02:55:48 +0700

    Add Search Request Detail

---
+++ Data/Models/SearchRequestDetailModel.cs
@@ -0,0 +1,31 @@
+﻿using Data.Model;
+using Microsoft.AspNetCore.Http;
+
+namespace Data.Models;
+
+public class SearchRequestDetailCreateModel
+{
+    public Guid SearchRequestId { get; set; }
+    public string? Name { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Note { get; set; }
+    public IFormFile? ImageData { get; set; }
+    public string? LicensePlate { get; set; }
+    public string? Brand { get; set; }
+    public string? Color { get; set; }
+    public IFormFile? VehicleImage { get; set; }
+}
+
+public class SearchRequestDetailModel
+{
+    public Guid Id { get; set; }
+    public SearchRequestModel SearchRequest { get; set; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Note { get; set; }
+    public string ImageData { get; set; }
+    public string LicensePlate { get; set; }
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public string VehicleImage { get; set; }
+}
