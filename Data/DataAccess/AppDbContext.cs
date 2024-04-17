0c2ce2b834e6b36701c1af7b0ab457bcdceca52b
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-04-17 15:26:49 +0700

    Revert "Remove SearchRequestDetail"
    This reverts commit 905ca66a24d2fb965c9e2ad4a0c4a1502497fa9e.

--- Data/DataAccess/AppDbContext.cs
+++ Data/DataAccess/AppDbContext.cs
@@ -81,5 +81,6 @@ public class AppDbContext : IdentityDbContext<User, Role, Guid>
     public DbSet<BookingPayment> BookingPayments { get; set; }
     public DbSet<BookingImage> BookingImages { get; set; }
     public DbSet<Support> Supports { get; set; }
+    public DbSet<SearchRequestDetail> SearchRequestDetails { get; set; }
     public DbSet<BookedPersonInfo> BookedPersonInfos { get; set; }
 }
