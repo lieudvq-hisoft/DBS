970f8dd82a6be6dfcb1feba5e00fbdfdf648420b
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-05-18 13:19:18 +0700

    Add CRD LinkedAcount

---
+++ DBS/Migrations/20240518050736_LinkedAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBS.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkedAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LinkedAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountNumber = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Brand = table.Column<string>(type: "text", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LinkedAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LinkedAccounts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkedAccounts_UserId",
+                table: "LinkedAccounts",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LinkedAccounts");
+        }
+    }
+}
---
+++ Data/Models/LinkedAccountModel.cs
@@ -0,0 +1,28 @@
+﻿using Data.Entities;
+using Data.Enums;
+using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class LinkedAccountCreateModel
+    {
+        public string AccountNumber { get; set; }
+        public LinkedAccountType Type { get; set; }
+        public string Brand { get; set; }
+    }
+
+    public class LinkedAccountModel
+    {
+        public Guid Id { get; set; }
+        public UserModel User { get; set; }
+        public string AccountNumber { get; set; }
+        public LinkedAccountType Type { get; set; }
+        public string Brand { get; set; }
+    }
+}
