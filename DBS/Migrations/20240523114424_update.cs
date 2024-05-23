1ec628de943ebd1d181b09036cf40602dd034aea
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-05-23 18:59:09 +0700

    Fix LinkedAccount in WalletTransaction

---
+++ DBS/Migrations/20240523114424_update.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBS.Migrations
+{
+    /// <inheritdoc />
+    public partial class update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkedAccountId",
+                table: "WalletTransactions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LinkedAccountId",
+                table: "WalletTransactions",
+                type: "uuid",
+                nullable: true);
+        }
+    }
+}
