7aa8cacdeb46b4c057755c558d1fae8fa88e4e5d
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-05-23 11:18:24 +0700

    Add migration

---
+++ DBS/Migrations/20240523041540_Add-LinkedImageUrl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLinkedImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedImgUrl",
+                table: "LinkedAccounts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkedImgUrl",
+                table: "LinkedAccounts");
+        }
+    }
+}
