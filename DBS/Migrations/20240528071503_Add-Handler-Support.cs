8bb385589cbb1dc749caba3711ed56808fa61490
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-05-28 15:07:09 +0700

    add handler support

---
+++ DBS/Migrations/20240528071503_Add-Handler-Support.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHandlerSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "HandlerId",
+                table: "Supports",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Supports_HandlerId",
+                table: "Supports",
+                column: "HandlerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Supports_AspNetUsers_HandlerId",
+                table: "Supports",
+                column: "HandlerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Supports_AspNetUsers_HandlerId",
+                table: "Supports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Supports_HandlerId",
+                table: "Supports");
+
+            migrationBuilder.DropColumn(
+                name: "HandlerId",
+                table: "Supports");
+        }
+    }
+}
