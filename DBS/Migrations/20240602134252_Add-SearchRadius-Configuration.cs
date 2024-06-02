e3484143c1b257e45b2f9d9702c14239d6b418d6
Huynh Trung Duong <trungduong22021619@gmail.com>
2024-06-02 20:46:49 +0700

    Add SearchRadius-Configuration

---
+++ DBS/Migrations/20240602134252_Add-SearchRadius-Configuration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSearchRadiusConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SearchRadius_Distance",
+                table: "PriceConfigurations",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SearchRadius_Unit",
+                table: "PriceConfigurations",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SearchRadius_Distance",
+                table: "PriceConfigurations");
+
+            migrationBuilder.DropColumn(
+                name: "SearchRadius_Unit",
+                table: "PriceConfigurations");
+        }
+    }
+}
